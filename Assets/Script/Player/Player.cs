e2ebe2985e0a65fab2cb2a4b2bb89d305bb7810b
binhnthe181882 <ngobinh0910ntb@gmail.com>
2026-03-10 02:19:28 +0700

    Create TileMap

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float moveSpeed = 8f;
+    private float jumpForce = 15f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    private bool isGrounded = true;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    void Start() { }
+
+    private void Update()
+    {
+        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        HandleMovement();
+        //HandleJump();
+        //HandleCombat();
     }
 
-    // Update is called once per frame
-    void Update()
+    //private void HandleJump()
+    //{
+    //    animator.SetBool("IsGrounded", isGrounded);
+
+    //    if (Input.GetButtonDown("Jump") && isGrounded)
+    //    {
+    //        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    //        animator.SetTrigger("Jump");
+    //    }
+    //}
+
+    private void HandleMovement()
     {
-        
+        float playerMoveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(playerMoveInput * moveSpeed, rb.velocity.y);
+
+        if (playerMoveInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (playerMoveInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        //if (playerInput != Vector2.zero)
+        //{
+        //    animator.SetBool("IsRunning", true);
+        //}
+        //else
+        //{
+        //    animator.SetBool("IsRunning", false);
+        //}
+
+        //animator.SetBool("IsRunning", moveInput != 0);
     }
+
+    //private void HandleCombat()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Z))
+    //    {
+    //        animator.SetTrigger("Attack");
+    //    }
+    //}
 }
