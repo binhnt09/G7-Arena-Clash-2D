4db4b9d88063561b5894cc8601b8b416d05d60fe
Hoang1012 <nhh10122004s@gmail.com>
2026-03-16 16:48:31 +0700

    Menu & Pause

---
+++ Assets/Script/UI/PauseManger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuPanel; 
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) Resume();
+            else Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuPanel.SetActive(true); 
+        Time.timeScale = 0f;          
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuPanel.SetActive(false); 
+        Time.timeScale = 1f;           
+        isPaused = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MenuScene");
+    }
+}
