d0bcad5f68f36ae5c67daafccba46e1089478d72
Hoang1012 <nhh10122004s@gmail.com>
2026-03-19 15:25:07 +0700

    Update Animation

--- Assets/Script/UI/MenuManager.cs
+++ Assets/Script/UI/MenuManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,6 +12,20 @@ public class MenuManager : MonoBehaviour
 
     public void PlayVsPlayer()
     {
-        // SceneManager.LoadScene("VersusScene");
+        SceneManager.LoadScene("PvsP_Scene");
     }
+    //    public static string selectedMode;
+
+    //    public void PlayVsAI()
+    //    {
+    //        selectedMode = "AI"; // Ghi nhớ chọn đánh máy
+    //        SceneManager.LoadScene("SelectCharacter"); // Chuyển đến màn chọn nhân vật
+    //    }
+
+    //    public void PlayVsPlayer()
+    //    {
+    //        selectedMode = "Player"; // Ghi nhớ chọn đánh người
+    //        SceneManager.LoadScene("SelectCharacter"); // Chuyển đến màn chọn nhân vật
+    //    }
+    //}
 }
